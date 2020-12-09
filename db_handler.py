c77f6af3fa225abd289c20dfc533740d07aa043e
George Francis <george.francais@tkh.edu.eg>
2020-12-09 14:12:27 +0200

    added the update to the DB handler

--- db_handler.py
+++ db_handler.py
@@ -5,6 +5,11 @@ class DB_handler:
     def __init__(self, js):
         with open(js) as db:
             self.memory = json.load(db)
-
+        self.db = js
     def print_memory(self):
         print(self.memory)
+
+    def update_memory(self):
+        with open(self.db, "w") as js:
+            if self.memory != json.load(js)
+            json.dump(self.memory,js)
